f6888a386daf8f0e735204183be70df3a4f292a7
danspav <danspav@nospam.com>
2018-11-06 19:30:44 +1100

    Saved changes

--- save.sh
+++ save.sh
@@ -2,5 +2,5 @@ cp /opt/splunk/etc/apps/departures-board-viz/local/data/ui/views/*.* /opt/git/de
 cp /opt/splunk/etc/apps/departures-board-viz/local/data/ui/nav/*.* /opt/git/departures-board-viz/default/data/ui/nav/
 git add .
 git commit -m "Saved changes"
-git push  https://danspav@gmail.com:K*%402*tMMGf*D6%40Tu@github.com/danspav/departures-board-viz.git
+git push https://danspav%40gmail.com:K*%402*tMMGf*D6%40Tu@github.com/danspav/departures-board-viz.git
 
