b0f183c9dec1b66d7d226a5913d24c0baad6eb84
danspav <42852307+danspav@users.noreply.github.com>
2018-11-08 19:24:12 +1100

    adf

--- appserver/static/visualizations/departures-board-viz/src/visualization_source.js
+++ appserver/static/visualizations/departures-board-viz/src/visualization_source.js
@@ -109,14 +109,14 @@ define([
 			var oDepartures_board = new departures_board();
 			oDepartures_board.setConfig(config, this.getPropertyNamespaceInfo().propertyNamespace, mvc);
 			oDepartures_board.setText(data)
-			var id=oDepartures_board.id;
+	//		var id=oDepartures_board.id;
 			this.$el.html(oDepartures_board.getHTML());
 			//var caption = oDepartures_board.getNextWord();
 			//window.jQuery("#" + id).flapper(oDepartures_board.getOpts());
 			//$("#" + id).val(caption).change();
-			var vizObj = this;	
-			var tokens={"term": {"key": token_word, "value": oDepartures_board.caption},"id": {"key": token_id, "value":oDepartures_board.value}}
-			vizObj.setTokens(tokens);
+	//		var vizObj = this;	
+	//		var tokens={"term": {"key": token_word, "value": oDepartures_board.caption},"id": {"key": token_id, "value":oDepartures_board.value}}
+	//		vizObj.setTokens(tokens);
 					
 			oDepartures_board.go();
 			/*
