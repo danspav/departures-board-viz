c1f35ab6bbb5754cb774a56f234353b9e8f51faf
danspav <42852307+danspav@users.noreply.github.com>
2018-11-25 13:38:22 +1100

    Delete save.sh

--- save.sh
+++
@@ -1,6 +0,0 @@
-cp /opt/splunk/etc/apps/departures-board-viz/local/data/ui/views/*.* /opt/git/departures-board-viz/default/data/ui/views/
-cp /opt/splunk/etc/apps/departures-board-viz/local/data/ui/nav/*.* /opt/git/departures-board-viz/default/data/ui/nav/
-git add .
-git commit -m "Saved changes"
-git push https://danspav%40gmail.com:K*%402*tMMGf*D6%40Tu@github.com/danspav/departures-board-viz.git
-
